60bf6992e8d6512bc89080325ccbb61764c41709
Sergio del Amo <sergio.delamo@softamo.com>
2025-02-15 18:56:16 +0100

    add develocity gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("io.micronaut.platform.catalog") version "4.4.5"
+    id("com.gradle.develocity") version("3.19.1")
 }
 
 rootProject.name = "micronaut-database-auth-parent"
@@ -8,3 +9,10 @@ include("micronaut-database-auth")
 include("code-coverage-report")
 include("docs")
 include("ui-tests")
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
