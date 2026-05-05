d234f893a47855ff8168ba26fcb10660b3231240
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-05 17:40:16 +0200

    exclude spock from geb

--- ui-tests/build.gradle.kts
+++ ui-tests/build.gradle.kts
@@ -11,7 +11,13 @@ dependencies {
     testImplementation(mn.micronaut.views.thymeleaf)
     testImplementation(mn.micronaut.http.server.netty)
     testCompileOnly(mn.micronaut.inject.groovy)
-    testImplementation(libs.geb.spock)
+
+    testImplementation(mn.micronaut.test.spock)
+    testImplementation(libs.geb.spock) {
+        exclude(group = "org.spockframework", module = "spock-core")
+        exclude(group = "org.apache.groovy.geb", module = "geb-implicit-assertions")
+    }
+
     testImplementation(libs.selenium.support)
     testImplementation(libs.selenium.api)
     testImplementation(libs.selenium.remote.driver)
